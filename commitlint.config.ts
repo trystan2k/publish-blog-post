2531cf21152f1f70c34af6af7577eb33e154fc48
Thiago Mendonca <trystan2k@gmail.com>
2025-01-15 23:43:56 +0100

    ci(config): update dependabot to only check dependencies in package.json

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -11,5 +11,6 @@ export default {
       return [2, 'always', [...subdirs, 'dist', 'config', 'deps', 'deps-dev', '*']];
     },
     'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 200],
   },
 };
