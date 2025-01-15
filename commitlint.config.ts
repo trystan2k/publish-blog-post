64996f4f32763072b50c493ed245a339f0af00e9
Thiago Mendonca <trystan2k@gmail.com>
2025-01-15 23:21:22 +0100

    ci(config): add dependabot workflow

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -8,7 +8,7 @@ export default {
       const srcDir = path.resolve(new URL('.', import.meta.url).pathname, 'src');
       const files = fs.readdirSync(srcDir, { withFileTypes: true });
       const subdirs = files.filter(file => file.isDirectory()).map(dir => dir.name);
-      return [2, 'always', [...subdirs, 'dist', 'config', '*']];
+      return [2, 'always', [...subdirs, 'dist', 'config', 'deps', 'deps-dev', '*']];
     },
     'header-max-length': [2, 'always', 100],
   },
